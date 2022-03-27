46cb3698d27c6779f1ecf2a6d92056dae15ca500
github username <github email address>
2022-03-27 16:26:53 -0400

    commit tree f4a66035d24985a7677c78954d81ab94a23bd900

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,14 @@
 def unsafe?(speed)
-    if (40<=speed<=60)
-        false
-    else
+    if 40>speed||speed>60
         true
+    else
+        false
     end
 
 end
 
 
-
-def not_safe?(speed)
-	
+def not_safe?
 end
 	
 
